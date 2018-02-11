1db17f51ce1961384cd4d1ae63832d345a48eca1
Robert Maxwell <Home@Roberts-MacBook-Pro.local>
2018-02-11 16:34:33 -0600

    Added packet rate functionality

--- AssignmentOne/DataReceiver.java
+++ AssignmentOne/DataReceiver.java
@@ -1,35 +1,41 @@
+/*
+ * Robert Maxwell
+ * CSCI 4311 - Spirng 2018
+ * Assignment One
+*/
 
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
 public class DataReceiver {
+
     public static void main(String[] args) throws IOException {
 
-		// get a datagram socket
-		 DatagramSocket socket = new DatagramSocket();
+		if (args.length != 3) {
+      System.out.println("Usage: java DataReceiver <ipAddress> <port> <interval>");
+      return;
+    }
 
-		// send request
+		//Send a packet to connect to the server
+		DatagramSocket socket = new DatagramSocket();
 		byte[] buf = new byte[256];
 		InetAddress address = InetAddress.getByName(args[0]);
 		int port = Integer.parseInt(args[1]);
+		int interval  = Integer.parseInt(args[2]);
 		DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
 		socket.send(packet);
-    
-        		// get response
+    socket.connect(address, port);
+
+    // get response
 		packet = new DatagramPacket(buf, buf.length);
-		
 		int packetCount = 0;
-		while(true)	{
+		while(socket.isConnected())	{
 			socket.receive(packet);
 			System.out.println("Received packet " + packetCount);
 			packetCount++;
 		}
-    
-        //socket.close();
+
+    socket.close();
     }
-} 
-
-
-
-
+}
--- AssignmentOne/DataSender.java
+++ AssignmentOne/DataSender.java
@@ -1,3 +1,8 @@
+/*
+ * Robert Maxwell
+ * CSCI 4311 - Spring 2018
+ * Assignment One
+*/
 
 import java.io.*;
 import java.net.*;
@@ -5,72 +10,82 @@ import java.util.*;
 
 public class DataSender extends Thread {
 
-    private DatagramSocket socket = null;
+    private DatagramSocket socket;
     private int interval;
-    private int packetsPerSecond;
+    private List<Integer> packetsPerSecond;
 
-    public DataSender(int port, int timeToSend, int packetRate) throws IOException {
-		socket = new DatagramSocket(port);
-		interval = timeToSend;
-		packetsPerSecond = packetRate;
+    public DataSender(int port, int timeToSend, List<Integer> packetRate) throws IOException {
+  		socket = new DatagramSocket(port);
+  		interval = timeToSend;
+  		packetsPerSecond = packetRate;
     }
-	
+
     public static void main(String[] args) throws IOException {
-		int port = Integer.parseInt(args[0]);
-		int timeToSend = Integer.parseInt(args[1]);
-		int packetRate = Integer.parseInt(args[2]);
-		try {
-		       Thread t = new DataSender(port, timeToSend, packetRate);
-		        t.start();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+  		if (args.length < 3) {
+        System.out.println("Usage: java DataSender <port> <inverval> <packetRate1> ... <packetRateN>");
+        return;
+      }
+
+  		int port = Integer.parseInt(args[0]);
+  		int timeToSend = Integer.parseInt(args[1]);
+  		List<Integer> packetRates = new ArrayList<>();
+  		for (int i = 2; i < args.length; i++)	{
+  			packetRates.add(Integer.parseInt(args[i]));
+  		}
+  		//int packetRate = Integer.parseInt(args[2]);
+  		try {
+        Thread t = new DataSender(port, timeToSend, packetRates);
+        t.start();
+  		} catch (IOException e) {
+  			e.printStackTrace();
+  		}
     }
 
     public void run() {
-        boolean keepgoing = true;
-        while (keepgoing) {
+        int numberOfPacketRates = packetsPerSecond.size();
+        int currentPacketRate = 0;
             try {
-                byte[] buf = new byte[256];
 
-                // receive request
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-	      System.out.println("Waiting for a connection...");
-                socket.receive(packet);
+              byte[] buf = new byte[256];
 
-                InetAddress address = packet.getAddress();
-                int port = packet.getPort();
-	       System.out.println("Recieved packet from:" + 
-			  "\nAddress: " + address +
-		           "\nPort: "+ port + "\n");
-		 
-                System.out.println("Sending Data..."); 
-                System.out.println("\nRate is: " + packetsPerSecond + " packets per second for " + interval + " seconds."); 
-				
-				for (int i = 0; i < interval; i++) 	{
-	                			packet = new DatagramPacket(buf, buf.length, address, port);
-	                			socket.send(packet);
-					try {
-						Thread.sleep(1000);
-					} catch (Exception e) {
-						System.out.println(e);
-					}
-				}
+              // receive a connection from a client
+              DatagramPacket packet = new DatagramPacket(buf, buf.length);
+              System.out.println("Waiting for a connection...\n");
+              socket.receive(packet);
 
+              InetAddress address = packet.getAddress();
+              int port = packet.getPort();
+              System.out.println("Recieved connection from:" +
+              "\nAddress: " + address +
+              "\nPort: "+ port + "\n");
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            } // finally{
-//             	socket.close();
-//             }
-        }
+              while (currentPacketRate < numberOfPacketRates) {
 
-    }
-    
-    
+                //Send data to client
+                System.out.println("Sending Data...");
+                System.out.println("Rate is: " + packetsPerSecond.get(currentPacketRate) + " packets per second for " + interval + " seconds.\n");
+                packet = new DatagramPacket(buf, buf.length, address, port);
+                for (int i = 0; i < interval; i++) 	{
+                  for (int j = 0; j < packetsPerSecond.get(currentPacketRate); j++) {
+                    socket.send(packet);
+                  }
+                  //socket.send(packet);
+                  try {
+                    Thread.sleep(1000);
+                  } catch (Exception e) {
+                    System.out.println(e);
+                  }
+                }
+
+                currentPacketRate++;
+              }//end while
+
+          } catch (IOException e) {
+            e.printStackTrace();
+          } finally {
+		          System.out.println("Goodbye.");
+              socket.close();
+          }
+      }
+
 }
-
-
-
-
-
