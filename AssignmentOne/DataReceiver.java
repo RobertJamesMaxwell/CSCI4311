7c282212a5da4fb86f0ac34d4139439d5db3d72a
Robert Maxwell <Home@Roberts-MacBook-Pro.local>
2018-02-18 15:12:41 -0600

    Changed from UDP to TCP. Added functionality for sending array of java ints as data

--- AssignmentOne/DataReceiver.java
+++ AssignmentOne/DataReceiver.java
@@ -7,13 +7,13 @@
 
 // To Do:
 // 1) Data to send should be array of 16 java ints?  Need to convert random int array to byte array?
-// 2) Possibly use Executor Service instead of Thread.sleep in the Data Sender?
-// 3) Need to account for extra packets when intervals don't match
-// 4) Need to adjust for port 121 - need admin rights SUDO
+
 
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class DataReceiver {
 
@@ -24,44 +24,85 @@ public class DataReceiver {
       return;
     }
 
-		//Send a packet to connect to the server
-		DatagramSocket socket = new DatagramSocket();
-		byte[] buf = new byte[256];
-		InetAddress address = InetAddress.getByName(args[0]);
+
+    String sentence;
+    String modifiedSentence;
+
+    InetAddress address = InetAddress.getByName(args[0]);
 		int port = Integer.parseInt(args[1]);
 		int interval  = Integer.parseInt(args[2]);
-		DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
-		socket.send(packet);
-    socket.connect(address, port);
+
+    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+    Socket clientSocket = new Socket(address, port);
+    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    sentence = inFromUser.readLine();
+    outToServer.writeBytes(sentence + '\n');
+    modifiedSentence = inFromServer.readLine();
+    System.out.println("FROM SERVER: " + modifiedSentence);
+
+    DataInputStream byteInFromServer = new DataInputStream(clientSocket.getInputStream());
+    byte[] buf = new byte[64];
+
+    while(true) {
+      int read = byteInFromServer.read(buf, 0, buf.length);
+      System.out.println("FROM SERVER: " + read);
+      System.out.println("Buffer:" + Arrays.toString(buf));
+
+      //ByteBuffer bb = ByteBuffer.allocate(64).put(buf);
+      //System.out.println("ByteBuffer:" + bb.toString());
+      for (int i=0; i < 64 ; i+=4) {
+        //int y = buf[i] << 24 | (buf[i+1] & 0xff) << 16 | (buf[i+2] & 0xff) << 8 | (buf[i+3] & 0xff);
+        //System.out.println("y = " + y);
+
+        ByteBuffer bb = ByteBuffer.wrap(buf);
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        //System.outbb.getInt();
+
+
+        int x = bb.getInt(i);
+        System.out.println("x = " + x);
+      }
+    }
+
+    //clientSocket.close();
+
+		//Send a packet to connect to the server
+		// DatagramSocket socket = new DatagramSocket();
+		// byte[] buf = new byte[256];
+
+		// DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+		// socket.send(packet);
+    // socket.connect(address, port);
 
     // get response
-		packet = new DatagramPacket(buf, buf.length);
-		int packetCount = 0;
-    long startTime = System.currentTimeMillis();
-    long currentTime = 0;
-    System.out.println("Receiving packets...\n");
-		while(socket.isConnected())	{
-			socket.receive(packet);
+		// packet = new DatagramPacket(buf, buf.length);
+		// int packetCount = 0;
+    // long startTime = System.currentTimeMillis();
+    // long currentTime = 0;
+    // System.out.println("Receiving packets...\n");
+		// while(socket.isConnected())	{
+		// 	socket.receive(packet);
+    //
+    //   //check if this is the last packet and disconnect from server if so
+    //   if (packet.getLength() == 8)  {
+    //     // if (packetCount > 0)  {
+    //     //   System.out.println("Received " + packetCount + " packets in " + (currentTime - startTime) / 1000 + " seconds.");
+    //     //   System.out.println("Received rate is: " + (packetCount / ((currentTime - startTime) / 1000)) + " packets per second for " + (currentTime - startTime) / 1000 + " seconds.\n");
+    //     // }
+    //     socket.disconnect();
+    //   }
+    //   packetCount++;
+    //   currentTime = System.currentTimeMillis();
+    //   if (currentTime - startTime > (interval*1000) ) {
+    //     System.out.println("Received " + packetCount + " packets in " + interval + " seconds.");
+    //     System.out.println("Received rate is: " + (packetCount / interval) + " packets per second for " + interval + " seconds.\n");
+    //     packetCount = 0;
+    //     startTime = System.currentTimeMillis();
+    //   }
+    //
+		// }
 
-      //check if this is the last packet and disconnect from server if so
-      if (packet.getLength() == 8)  {
-        // if (packetCount > 0)  {
-        //   System.out.println("Received " + packetCount + " packets in " + (currentTime - startTime) / 1000 + " seconds.");
-        //   System.out.println("Received rate is: " + (packetCount / ((currentTime - startTime) / 1000)) + " packets per second for " + (currentTime - startTime) / 1000 + " seconds.\n");
-        // }
-        socket.disconnect();
-      }
-      packetCount++;
-      currentTime = System.currentTimeMillis();
-      if (currentTime - startTime > (interval*1000) ) {
-        System.out.println("Received " + packetCount + " packets in " + interval + " seconds.");
-        System.out.println("Received rate is: " + (packetCount / interval) + " packets per second for " + interval + " seconds.\n");
-        packetCount = 0;
-        startTime = System.currentTimeMillis();
-      }
-
-		}
-
-    socket.close();
+    //socket.close();
     }
 }
--- AssignmentOne/DataSender.java
+++ AssignmentOne/DataSender.java
@@ -10,87 +10,146 @@ import java.util.*;
 
 public class DataSender extends Thread {
 
-    private DatagramSocket socket;
-    private int interval;
-    private List<Integer> packetsPerSecond;
+    //private static ServerSocket serverSocket;
+    // public DataSender(int port, int timeToSend, List<Integer> packetRate) throws IOException {
+  	// 	serverSocket = new ServerSocket(port);
+  	// 	interval = timeToSend;
+  	// 	packetsPerSecond = packetRate;
+    // }
 
-    public DataSender(int port, int timeToSend, List<Integer> packetRate) throws IOException {
-  		socket = new DatagramSocket(port);
-  		interval = timeToSend;
-  		packetsPerSecond = packetRate;
-    }
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws IOException {
+      //error message to user about correct usage of DataSender
   		if (args.length < 3) {
         System.out.println("Usage: java DataSender <port> <inverval> <packetRate1> ... <packetRateN>");
         return;
       }
 
+      //get port, intever to send packets, and variable number of packet rates from command line
   		int port = Integer.parseInt(args[0]);
   		int timeToSend = Integer.parseInt(args[1]);
   		List<Integer> packetRates = new ArrayList<>();
   		for (int i = 2; i < args.length; i++)	{
   			packetRates.add(Integer.parseInt(args[i]));
   		}
-  		//int packetRate = Integer.parseInt(args[2]);
-  		try {
-        Thread t = new DataSender(port, timeToSend, packetRates);
-        t.start();
-  		} catch (IOException e) {
-  			e.printStackTrace();
-  		}
-    }
 
-    public void run() {
-        int numberOfPacketRates = packetsPerSecond.size();
-        int currentPacketRate = 0;
-            try {
+      //establish server running on port given as args[0]
+      ServerSocket serverSocket = new ServerSocket(port);
+      System.out.println("\r\nRunning Server: " +
+        "\n Host=" + serverSocket.getInetAddress().getHostAddress() +
+        "\n Port=" + serverSocket.getLocalPort());
 
-              byte[] buf = new byte[256];
+      //get connection from a client
+      Socket client = serverSocket.accept();
+      System.out.println("\r\nNew connection from: " +
+        "\n Host=" + client.getInetAddress().getHostAddress() +
+        "\n Port=" + client.getLocalPort());
 
-              // receive a connection from a client
-              DatagramPacket packet = new DatagramPacket(buf, buf.length);
-              System.out.println("Waiting for a connection...\n");
-              socket.receive(packet);
 
-              InetAddress address = packet.getAddress();
-              int port = packet.getPort();
-              System.out.println("Recieved connection from:" +
-              "\nAddress: " + address +
-              "\nPort: "+ port + "\n");
+      BufferedReader inFromClient =
+          new BufferedReader(new InputStreamReader(client.getInputStream()));
+      DataOutputStream outToClient = new DataOutputStream(client.getOutputStream());
+      String clientSentence = inFromClient.readLine();
+      System.out.println("\nReceived: " + clientSentence);
+      String capitalizedSentence = clientSentence.toUpperCase() + '\n';
+      outToClient.writeBytes(capitalizedSentence + '\n');
 
-              while (currentPacketRate < numberOfPacketRates) {
-
-                //Send data to client
-                System.out.println("Sending Data...");
-                System.out.println("Rate is: " + packetsPerSecond.get(currentPacketRate) + " packets per second for " + interval + " seconds.\n");
-                packet = new DatagramPacket(buf, buf.length, address, port);
-                for (int i = 0; i < interval; i++) 	{
-                  for (int j = 0; j < packetsPerSecond.get(currentPacketRate); j++) {
-                    socket.send(packet);
-                  }
-                  //socket.send(packet);
-                  try {
-                    Thread.sleep(1000);
-                  } catch (Exception e) {
-                    System.out.println(e);
-                  }
-                }
-
-                currentPacketRate++;
-              }//end while
-
-              //Send a signal to client that the process is over
-              buf = new byte[8];
-              packet = new DatagramPacket(buf, buf.length, address, port);
-              socket.send(packet);
-
-          } catch (IOException e) {
-            e.printStackTrace();
-          } finally {
-		          System.out.println("Goodbye.");
-              socket.close();
-          }
+      //prep data to send
+      int[] data = {43, 11, 14, 31, 13, 44, 55, 66, 77, 88, 99, 76, 54, 32, 1, 2};
+      byte[] buf = new byte[64];
+      System.out.println("Buffer:" + Arrays.toString(buf));
+      for (int i = 0; i < data.length; i++) {
+        //System.out.println("Data at i:" + data[i]);
+        Integer integer = data[i];
+        //System.out.println("Buff at i*4:" + buf[i*4]);
+        buf[i*4] = integer.byteValue();
+        //System.out.println("Buff at i*4:" + buf[i*4]);
       }
 
+      System.out.println("Buffer:" + Arrays.toString(buf));
+
+      while (true)  {
+        System.out.println("Writing data");
+        outToClient.write(buf, 0, buf.length);
+        Thread.sleep(1000);
+      }
+
+
+  		// try {
+      //   Thread t = new DataSender(port, timeToSend, packetRates);
+      //   t.start();
+  		// } catch (IOException e) {
+  		// 	e.printStackTrace();
+  		// }
+    }
+
+
+
 }
+
+        // BufferedReader in = new BufferedReader(
+        //         new InputStreamReader(client.getInputStream()));
+        // while ( (data = in.readLine()) != null ) {
+        //     System.out.println("\r\nMessage from " + clientAddress + ": " + data);
+        // }
+
+
+      //   int numberOfPacketRates = packetsPerSecond.size();
+      //   int currentPacketRate = 0;
+      //       try {
+      //
+      //         byte[] buf = new byte[256];
+      //
+      //         // List<Integer> randomData = new ArrayList<Integer>();
+      //         // for (int i = 0; i < 16; i++ ) {
+      //         //   randomData.add(i);
+      //         // }
+      //
+      //
+      //
+      //         buf = new
+      //
+      //         // receive a connection from a client
+      //         DatagramPacket packet = new DatagramPacket(buf, buf.length);
+      //         System.out.println("Waiting for a connection...\n");
+      //         socket.receive(packet);
+      //
+      //         InetAddress address = packet.getAddress();
+      //         int port = packet.getPort();
+      //         System.out.println("Recieved connection from:" +
+      //         "\nAddress: " + address +
+      //         "\nPort: "+ port + "\n");
+      //
+      //         while (currentPacketRate < numberOfPacketRates) {
+      //
+      //           //Send data to client
+      //           System.out.println("Sending Data...");
+      //           System.out.println("Rate is: " + packetsPerSecond.get(currentPacketRate) + " packets per second for " + interval + " seconds.\n");
+      //           packet = new DatagramPacket(buf, buf.length, address, port);
+      //           for (int i = 0; i < interval; i++) 	{
+      //             for (int j = 0; j < packetsPerSecond.get(currentPacketRate); j++) {
+      //               socket.send(packet);
+      //             }
+      //             //socket.send(packet);
+      //             try {
+      //               Thread.sleep(1000);
+      //             } catch (Exception e) {
+      //               System.out.println(e);
+      //             }
+      //           }
+      //
+      //           currentPacketRate++;
+      //         }//end while
+      //
+      //         //Send a signal to client that the process is over
+      //         buf = new byte[8];
+      //         packet = new DatagramPacket(buf, buf.length, address, port);
+      //         socket.send(packet);
+      //
+      //     } catch (IOException e) {
+      //       e.printStackTrace();
+      //     } finally {
+		  //         System.out.println("Goodbye.");
+      //         socket.close();
+      //     }
+      // }
