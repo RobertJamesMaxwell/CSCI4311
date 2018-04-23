fa890d9a5274d2c86644755782d740d041f648ef
Robert Maxwell <Home@Roberts-MacBook-Pro.local>
2018-04-22 22:29:11 -0500

    Updated Assignment One 2.0

---
+++ 2.0/AssignmentOne2.0/DataReceiver.java
@@ -0,0 +1,137 @@
+/*
+ * Robert Maxwell
+ * CSCI 4311 - Spring 2018
+ * Assignment One
+*/
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.lang.Math;
+
+public class DataReceiver {
+
+    public static void main(String[] args) throws IOException {
+
+
+      //error message to user about correct usage of DataSender
+      if (args.length != 3) {
+        System.out.println("Usage: java DataReceiver <ipAddress> <port> <interval>");
+        return;
+      }
+
+
+      //get IP address, port, and interval to accept packets from command line
+      InetAddress address = InetAddress.getByName(args[0]);
+      int port = Integer.parseInt(args[1]);
+
+
+      //establish connection to server
+      Socket clientSocket = new Socket(address, port);
+      BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      String successfulConnection = inFromServer.readLine();
+      System.out.println("=====================================================");
+      System.out.println("\nData Receiver has successfully connected to a Data Sender Server and received the following message:");
+      System.out.println("'"+ successfulConnection + "'\n");
+      System.out.println("=====================================================");
+
+
+      //accept data from server
+      DataInputStream byteInFromServer = new DataInputStream(clientSocket.getInputStream());
+      byte[] buf = new byte[64];
+
+
+      //read first two packets to establish the rate
+      //first packet
+      int numberOfReadBytes = byteInFromServer.read(buf, 0, buf.length);
+      long timeOfPacketReceived = System.currentTimeMillis();
+      long currentPacketRateStartTime = System.currentTimeMillis();
+      long timeOfPreviousPacket = timeOfPacketReceived;
+
+      //second packet
+      numberOfReadBytes = byteInFromServer.read(buf, 0, buf.length);
+      timeOfPacketReceived = System.currentTimeMillis();
+      long timeBetweenPackets = timeOfPacketReceived - timeOfPreviousPacket;
+      long timeBetweenPreviousPackets = timeBetweenPackets;
+      timeOfPreviousPacket = timeOfPacketReceived;
+
+      System.out.println("Receiving packets...");
+      boolean newPacketRate = false;
+      long currentPacketRateEndTime = 0;
+      int numberOfPacketsReceived = 2;
+      int packetRateNumber = 1;
+
+      while(true) {
+
+        //read a packet of bytes
+        numberOfReadBytes = byteInFromServer.read(buf, 0, buf.length);
+
+        //calculate packet rates
+        timeOfPacketReceived = System.currentTimeMillis();
+        timeBetweenPackets = timeOfPacketReceived - timeOfPreviousPacket;
+        timeOfPreviousPacket = timeOfPacketReceived;
+
+        //allow for error of 10ms in calculation
+        if ( (Math.abs(timeBetweenPackets - timeBetweenPreviousPackets) > 10) ) {
+          newPacketRate = true;
+        }
+        timeBetweenPreviousPackets = timeBetweenPackets;
+
+
+        //account for if server has stopped sending packetRates
+        if (numberOfReadBytes == 1) {
+          newPacketRate = true;
+        }
+
+
+        //if the rate of packets changes, alert user, print previous packet rate, and print out the data to make sure it's still correct
+        if(newPacketRate)   {
+          System.out.println("\n\n-----------------------------------------");
+          currentPacketRateEndTime = System.currentTimeMillis();
+          System.out.println("PACKET RATE HAS CHANGED");
+          System.out.println("Previous packet rate was: " +
+            numberOfPacketsReceived + " packets received in " +
+                  (double)(currentPacketRateEndTime - currentPacketRateStartTime) / 1000 + " seconds." );
+          System.out.println("Packet Rate " + packetRateNumber + " = "
+                  + ((double)numberOfPacketsReceived / ((double)(currentPacketRateEndTime - currentPacketRateStartTime) / 1000) ) + " packets per second.");
+          currentPacketRateStartTime = currentPacketRateEndTime;
+          numberOfPacketsReceived = 0;
+          newPacketRate = false;
+
+          //convert bytes read back into ints to make sure we got the correct data
+          System.out.println("Bytes read from Server: " + numberOfReadBytes);
+          System.out.println("Buffer received is:\n" + Arrays.toString(buf));
+          List<Integer> data = new ArrayList<Integer>();
+          for (int i=0; i < 64 ; i+=4) {
+            ByteBuffer bb = ByteBuffer.wrap(buf);
+            bb.order(ByteOrder.LITTLE_ENDIAN);
+            int x = bb.getInt(i);
+            data.add(x);
+          }
+          System.out.println("Data received is: " + data.toString());
+          System.out.println("-----------------------------------------\n\n");
+
+          //exit loop if received a packet of 1 bytes (1 empty byte is signal from server that process is over)
+          if (numberOfReadBytes == 1) {
+            System.out.println("Done receiving.");
+            System.out.println("Goodbye.");
+            break;
+          }
+          System.out.println("Receiving packets...");
+          packetRateNumber++;
+        }
+
+        numberOfPacketsReceived++;
+
+
+    } // end while
+
+    clientSocket.close();
+    }
+
+    public static void calculateRate()  {
+
+    }
+}
---
+++ 2.0/AssignmentOne2.0/DataSender.java
@@ -0,0 +1,130 @@
+/*
+ * Robert Maxwell
+ * CSCI 4311 - Spring 2018
+ * Assignment One
+*/
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class DataSender {
+
+    public static void main(String[] args) {
+
+
+      //error message to user about correct usage of DataSender
+  		if (args.length < 3) {
+        System.out.println("Usage: java DataSender <port> <inverval> <packetRate1> <packetRate2> ... <packetRateN>");
+        return;
+      }
+
+
+      //get port, interval to send packets, and variable number of packet rates from command line
+  		int port = Integer.parseInt(args[0]);
+  		int interval = Integer.parseInt(args[1]);
+  		List<Integer> packetRates = new ArrayList<>();
+  		for (int i = 2; i < args.length; i++)	{
+  			packetRates.add(Integer.parseInt(args[i]));
+  		}
+
+
+      //establish server running on port given as args[0]
+      ServerSocket serverSocket = null;
+      try {
+        serverSocket = new ServerSocket(port);
+      } catch (IOException ioe) {
+        ioe.printStackTrace();
+      }
+      System.out.println("=====================================================");
+      System.out.println("Hello, Data Sender has Started.");
+      System.out.println("\r\nRunning Server: " +
+        "\nHost = " + (serverSocket != null ? serverSocket.getInetAddress().getHostAddress() : null) +
+        "\nPort = " + (serverSocket != null ? serverSocket.getLocalPort() : 0));
+      System.out.println("=====================================================");
+
+      //get connection from a client
+      Socket client = null;
+      try {
+        client = serverSocket != null ? serverSocket.accept() : null;
+      } catch (IOException ioe) {
+        ioe.printStackTrace();
+      }
+      System.out.println("=====================================================");
+      System.out.println("New connection from: " +
+        "\nHost = " + (client != null ? client.getInetAddress().getHostAddress() : null) +
+        "\nPort = " + (client != null ? client.getLocalPort() : 0));
+      System.out.println("=====================================================");
+
+      //establish output stream to client
+      String welcomeMessageToClient = "Hello, thanks for connecting to the Data Sender Server. \t"
+              + "Preparing data to send...";
+      DataOutputStream outToClient = null;
+      try {
+        outToClient = new DataOutputStream(client != null ? client.getOutputStream() : null);
+        outToClient.writeBytes(welcomeMessageToClient + '\n');
+      } catch (IOException ioe) {
+        ioe.printStackTrace();
+      }
+
+
+      //prep data to send
+      int[] data = {43, 11, 14, 31, 13, 44, 55, 66, 77, 88, 99, 76, 54, 32, 1, 2};
+      byte[] buf = new byte[64];
+      for (int i = 0; i < data.length; i++) {
+        Integer integer = data[i];
+        buf[i*4] = integer.byteValue();
+      }
+      System.out.println("=====================================================");
+      System.out.println("Data to send is:\n" + Arrays.toString(data));
+      System.out.println("\nBuffer to send is:\n" + Arrays.toString(buf));
+      System.out.println("=====================================================");
+
+
+      //send data for each packet rate
+      int numberOfPacketRates = packetRates.size();
+      int currentPacketRateIndex = 0;
+      while (currentPacketRateIndex < numberOfPacketRates)  {
+        int currentPacketRate = packetRates.get(currentPacketRateIndex);
+        System.out.println("=====================================================");
+        System.out.println("\nWriting data at " + currentPacketRate + " packets per second for " + interval + " seconds...\n");
+        System.out.println("=====================================================");
+        for (int i = 0; i < interval; i++) 	{
+          for (int j = 0; j < currentPacketRate; j++) {
+            try {
+              if (outToClient != null) {
+                outToClient.write(buf, 0, buf.length);
+              }
+              Thread.sleep(1000 / currentPacketRate);
+            } catch (IOException | InterruptedException ioe) {
+              ioe.printStackTrace();
+            }
+
+          }
+        }
+        currentPacketRateIndex++;
+      } //end while
+
+
+      //send a signal to client that the process is over
+      //one packet of a different size (size 1) to denote the 'last packet'
+      buf = new byte[1];
+      try {
+        if (outToClient != null) {
+          outToClient.write(buf, 0, buf.length);
+        }
+        if (client != null) {
+          client.close();
+        }
+        if (serverSocket != null) {
+          serverSocket.close();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      System.out.println("=====================================================");
+      System.out.println("\nData Sender Server is finished and shutting down. \nGoodbye.");
+    }
+
+}
