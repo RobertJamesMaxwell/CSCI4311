92a2f0197e3545597714261eeff479160c7277a9
Robert Maxwell <Home@Roberts-MacBook-Pro.local>
2018-04-29 19:39:23 -0500

    Cleaned up Control Plane prints, comments

--- AssignmentFour/src/dataplane/forwarding/ControlPlane.java
+++ AssignmentFour/src/dataplane/forwarding/ControlPlane.java
@@ -20,19 +20,14 @@ public class ControlPlane {
 
     private static DataPlanePort dataPlanePort;
     private static List<PortUser> portUserList = new ArrayList<>();
-    private static List<HashMap<Integer, Integer>> entireForwardingTable = new ArrayList<>();
     private static int dvNodePortNumber;
-    //public static boolean needToChange = false;
     public static DVNode nodeFromCoordinator;
     public static HashMap<Integer, Integer> realForwardingTable = new HashMap<>();
     public static DVSender dvSender;
 
     public static void main(String[] args) {
 
-        //setup forwarding table
-        setupForwardingTable();
-
-
+        // default port number
         dvNodePortNumber = 12110;
 
         try {
@@ -80,6 +75,7 @@ public class ControlPlane {
             System.out.println("\n\nReceived the following Port neighbor mapping from server!");
             System.out.println(neighborNodesPortList);
 
+            //create port users and count your number of neighbors
             int numberOfNeighbors = 0;
             for (int i = 0; i < nodeFromCoordinator.dv.length; i++) {
                 if (nodeFromCoordinator.dv[i] != Integer.MAX_VALUE && nodeFromCoordinator.dv[i] != 0) {
@@ -114,8 +110,6 @@ public class ControlPlane {
             System.out.println("My Real Forwarding Table is: " + realForwardingTable);
 
 
-
-
             // setup DV Sender
             dvSender = new DVSender();
             int multicastSendPort = dvNodePortNumber + nodeFromCoordinator.nodeNum;
@@ -126,75 +120,40 @@ public class ControlPlane {
             dvSender.setMulticastIPNumber(multicastSendIP);
 
 
-            // setup DV Receiver --needs to be threaded???
+            // setup DV Receiver
             MulticastSocket socketForListening = new MulticastSocket(12345);
-            //List<MulticastSocket> socketsForListening = new ArrayList<>();
             for (int i = 0; i < neighborNodesIPList.size(); i++){
                 // if IP is empty at this index, then we don't need to listen to that neighbor
                 if (!neighborNodesIPList.get(i).equals(""))  {
-                    //MulticastSocket listeningSocket = new MulticastSocket(dvNodePortNumber + i);
                     socketForListening.joinGroup(InetAddress.getByName("230.0.0." + Integer.toString(i)));
-                    //socketsForListening.add(listeningSocket);
                 }
             }
-
             DVReceiver dvReceiver = new DVReceiver(socketForListening);
-            //dvReceiver.setSocketsForListening(socketsForListening);
 
+            // start DV Receiver thread to be ready to listen for sends from DV Sender
             dvReceiver.start();
 
+
+            // do one send to your neighbors to kick off the network setup
+            // when DV Receiver receives, it will also do a send if it has updated Control Plane's DV and forwarding table
             DV dvToSendOverNetwork = new DV(nodeFromCoordinator.nodeNum);
             dvToSendOverNetwork.setDv(nodeFromCoordinator.dv);
             dvSender.send(dvToSendOverNetwork);
 
 
-            //send your DV Object to your neighbors so they can update their tables -- loop this
-
-            //if DV Receiver updates your own DV based on what it receives, re-update your DV and send it
-            //int numberOfSendsToSetupNetwork = 0;
+            // wait for your forwarding table to be full before you are able to forward messages
+            // note, when the forwarding table is full, you might not yet have the shortest path, but that will still be working itself out
             while (realForwardingTable.containsValue(null))    {
                 System.out.println("Waiting for network to be setup.");
                 Thread.sleep(2000);
-
-
-                //send your DV Object to your neighbors so they can update their tables -- loop this
-//                System.out.println("Sending and sleeping for 2 seconds");
-//                dvSender.send(dvToSendOverNetwork);
-//                Thread.sleep(2 * 1000);
-                //if (needToChange)   {
-                  //  needToChange = false;
-                   // System.out.println("CHAAANGE NUMBER " + loopCount);
-                    //update your forwarding table
-                   // DV newDV = DVReceiver.receivedDV;
-
-
-
-//                    System.out.println("Sleeping for 10 seconds");
-//                    Thread.sleep(10*1000);
-                    //check if your forwarding table is full, if it is, then break, because you're ready to move to the next loop to listening for traffic
-
-
-//                if (realForwardingTable.containsValue(null))  {
-//                    System.out.println("Still don't have all values...");
-//                } else {
-//                    System.out.println("Sending extra send");
-//                    //for (int i = 0; i < 100; i++)   {
-//                        dvSender.send(dvToSendOverNetwork);
-//                    //    Thread.sleep(30);
-//                    //}
-//                    System.out.println("Breaking!");
-//                    break;
-//                }
-
-                //}
-                //numberOfSendsToSetupNetwork++;
             } //end while
 
 
-//            System.out.println("SLEEPING FOREVER");
-//            Thread.sleep(100 * 60 * 1000);
+            // forwarding table is setup, but sleep a little extra in case a faster path is found
+            Thread.sleep(2000);
 
-            //dvReceiver.stop();
+
+            // DV Network is setup
             System.out.println("========================================================");
             System.out.println("========================================================");
             System.out.println("========================================================");
@@ -207,7 +166,6 @@ public class ControlPlane {
             System.out.println("========================================================");
             System.out.println("========================================================");
             System.out.println("DV NETWORK IS SETUP!!!!");
-            //System.out.println("Number of Sends needed to setup network: " + numberOfSendsToSetupNetwork);
             System.out.println("I am Node: " + nodeFromCoordinator.nodeNum);
             System.out.println("My DV's are:");
             for(int j: nodeFromCoordinator.dv) {
@@ -218,14 +176,10 @@ public class ControlPlane {
             System.out.println(realForwardingTable);
 
 
-
-
-
-            //get this node's forwarding table
-            //HashMap<Integer, Integer> myForwardingTable = entireForwardingTable.get(nodeFromCoordinator.nodeNum);
+            // get this node's forwarding table
             HashMap<Integer, Integer> myForwardingTable = realForwardingTable;
 
-            //listen for incoming connections
+            // listen for incoming connections
             while (true) {
                 System.out.println("\nListening for a message...");
                 MessageType receivedMessage = dataPlanePort.receive();
@@ -245,15 +199,10 @@ public class ControlPlane {
                     //find the correct portuser who is connected to the correct neighbor node
                     for (PortUser portUser : portUserList) {
                         if (portUser.nodeId == nodeToForwardTo) {
-                            System.out.println("This is the correct PortUser to forward to: " + portUser.nodeId);
-                            System.out.println("Forwarding to next node: " + nodeToForwardTo);
                             portUser.send(msg);
                         }
                     }
                     Thread.sleep(1000);
-
-                    //update your DV because cost has changeed?
-                    //if cost has changed enough, do DVSender.send
                 }
             }
 
@@ -271,127 +220,4 @@ public class ControlPlane {
 
     }
 
-    private static void setupForwardingTable() {
-
-        //node 0
-        HashMap<Integer, Integer> nodeZero = new HashMap<Integer, Integer>();
-        nodeZero.put(0,0);
-        nodeZero.put(1,1);
-        nodeZero.put(2,3);
-        nodeZero.put(3,3);
-        nodeZero.put(4,3);
-        nodeZero.put(5,3);
-        nodeZero.put(6,3);
-        nodeZero.put(7,1);
-        nodeZero.put(8,8);
-        entireForwardingTable.add(nodeZero);
-
-        //node 1
-        HashMap<Integer, Integer> nodeOne = new HashMap<Integer, Integer>();
-        nodeOne.put(0,0);
-        nodeOne.put(1,1);
-        nodeOne.put(2,7);
-        nodeOne.put(3,0);
-        nodeOne.put(4,0);
-        nodeOne.put(5,7);
-        nodeOne.put(6,7);
-        nodeOne.put(7,7);
-        nodeOne.put(8,0);
-        entireForwardingTable.add(nodeOne);
-
-        //node 2
-        HashMap<Integer, Integer> nodeTwo = new HashMap<Integer, Integer>();
-        nodeTwo.put(0,3);
-        nodeTwo.put(1,7);
-        nodeTwo.put(2,2);
-        nodeTwo.put(3,3);
-        nodeTwo.put(4,3);
-        nodeTwo.put(5,5);
-        nodeTwo.put(6,5);
-        nodeTwo.put(7,7);
-        nodeTwo.put(8,3);
-        entireForwardingTable.add(nodeTwo);
-
-        //node 3
-        HashMap<Integer, Integer> nodeThree = new HashMap<Integer, Integer>();
-        nodeThree.put(0,0);
-        nodeThree.put(1,0);
-        nodeThree.put(2,2);
-        nodeThree.put(3,3);
-        nodeThree.put(4,4);
-        nodeThree.put(5,2);
-        nodeThree.put(6,2);
-        nodeThree.put(7,2);
-        nodeThree.put(8,0);
-        entireForwardingTable.add(nodeThree);
-
-        //node 4
-        HashMap<Integer, Integer> nodeFour = new HashMap<Integer, Integer>();
-        nodeFour.put(0,3);
-        nodeFour.put(1,3);
-        nodeFour.put(2,3);
-        nodeFour.put(3,3);
-        nodeFour.put(4,4);
-        nodeFour.put(5,3);
-        nodeFour.put(6,3);
-        nodeFour.put(7,3);
-        nodeFour.put(8,8);
-        entireForwardingTable.add(nodeFour);
-
-        //node 5
-        HashMap<Integer, Integer> nodeFive = new HashMap<Integer, Integer>();
-        nodeFive.put(0,2);
-        nodeFive.put(1,2);
-        nodeFive.put(2,2);
-        nodeFive.put(3,2);
-        nodeFive.put(4,2);
-        nodeFive.put(5,5);
-        nodeFive.put(6,6);
-        nodeFive.put(7,2);
-        nodeFive.put(8,2);
-        entireForwardingTable.add(nodeFive);
-
-        //node 6
-        HashMap<Integer, Integer> nodeSix = new HashMap<Integer, Integer>();
-        nodeSix.put(0,5);
-        nodeSix.put(1,5);
-        nodeSix.put(2,5);
-        nodeSix.put(3,5);
-        nodeSix.put(4,5);
-        nodeSix.put(5,5);
-        nodeSix.put(6,6);
-        nodeSix.put(7,5);
-        nodeSix.put(8,5);
-        entireForwardingTable.add(nodeSix);
-
-        //node 7
-        HashMap<Integer, Integer> nodeSeven = new HashMap<Integer, Integer>();
-        nodeSeven.put(0,1);
-        nodeSeven.put(1,1);
-        nodeSeven.put(2,2);
-        nodeSeven.put(3,2);
-        nodeSeven.put(4,2);
-        nodeSeven.put(5,2);
-        nodeSeven.put(6,2);
-        nodeSeven.put(7,7);
-        nodeSeven.put(8,1);
-        entireForwardingTable.add(nodeSeven);
-
-        //node 8
-        HashMap<Integer, Integer> nodeEight = new HashMap<Integer, Integer>();
-        nodeEight.put(0,0);
-        nodeEight.put(1,0);
-        nodeEight.put(2,0);
-        nodeEight.put(3,0);
-        nodeEight.put(4,0);
-        nodeEight.put(5,0);
-        nodeEight.put(6,0);
-        nodeEight.put(7,0);
-        nodeEight.put(8,8);
-        entireForwardingTable.add(nodeEight);
-    }
-
-    public boolean changed() {
-        return true;
-    }
 }
